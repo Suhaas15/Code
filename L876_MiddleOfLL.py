0add4bf8de881f1f578799b13731c13ece7382a4
Suhaas <53684099+Suhaas15@users.noreply.github.com>
2025-05-11 02:54:10 -0700

    Update L876_MiddleOfLL.py

--- L876_MiddleOfLL.py
+++ L876_MiddleOfLL.py
@@ -5,20 +5,25 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        current=head
-        pos=0
-        while current.next:
-            current=current.next
-            pos+=1
+        # current=head                  #brute force
+        # pos=0
+        # while current.next:
+        #     current=current.next
+        #     pos+=1
         
-        mid = (pos+1)//2
+        # mid = (pos+1)//2
 
-        current=head
-        pos=0
-        while current:
-            if pos==mid:
-                return current
-            current=current.next
-            pos+=1
+        # current=head
+        # pos=0
+        # while current:
+        #     if pos==mid:
+        #         return current
+        #     current=current.next
+        #     pos+=1
         
+        slow,fast=head,head
+        while fast and fast.next and slow:
+            slow=slow.next
+            fast=fast.next.next
+        return slow
         
