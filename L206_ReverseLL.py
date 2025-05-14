41c96b4a0077987f9261eec397c625cd0da4463f
Suhaas <53684099+Suhaas15@users.noreply.github.com>
2025-05-14 12:54:51 -0700

    Update L206_ReverseLL.py

--- L206_ReverseLL.py
+++ L206_ReverseLL.py
@@ -5,12 +5,23 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        node = None
+        # node = None           #iterative
 
-        while head:
-            temp=head.next
-            head.next=node
-            node=head
-            head=temp
+        # while head:
+        #     temp=head.next
+        #     head.next=node
+        #     node=head
+        #     head=temp
         
-        return node
+        # return node
+        if head is None or head.next is None:
+            return head
+        
+        new_head = self.reverseList(head.next)
+
+        front=head.next
+        front.next=head
+        head.next=None
+
+        return new_head
+
