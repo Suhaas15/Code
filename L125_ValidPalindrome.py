1324f0078fc6b9954bc420dc8b01cfa3114b6ff2
Suhaas <53684099+Suhaas15@users.noreply.github.com>
2025-06-23 17:37:59 -0700

    Add files via upload

---
+++ L125_ValidPalindrome.py
@@ -0,0 +1,36 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s)==1:
+            return True
+
+        # string = ''.join(c.lower() for c in s if c.isalnum())         #with new string
+        
+        # left=0
+        # right=len(string)-1
+
+        # while left<right:
+        #     if string[left]!=string[right]:
+        #         return False
+        #     left+=1
+        #     right-=1
+        
+        # return True
+
+        left=0
+        right=len(s)-1
+
+        while left<right:
+            while left<right and not s[left].isalnum():
+                left+=1
+            while left<right and not s[right].isalnum():
+                right-=1
+            
+            if s[left].lower()!=s[right].lower():
+                return False
+            
+            left+=1
+            right-=1
+        
+        return True
+
+
