adf48ffdd625db3aea9a9eec52d1beb4c92a5bf4
Suhaas <53684099+Suhaas15@users.noreply.github.com>
2025-07-22 16:02:25 -0700

    Update L145_BinaryTreePostOrder.py

--- L145_BinaryTreePostOrder.py
+++ L145_BinaryTreePostOrder.py
@@ -8,13 +8,34 @@ class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         res=[]
 
-        def post(node):
-            if not node:
-                return 
+        # def post(node):
+        #     if not node:
+        #         return 
             
-            post(node.left)
-            post(node.right)
-            res.append(node.val)
+        #     post(node.left)
+        #     post(node.right)
+        #     res.append(node.val)
         
-        post(root)
+        # post(root)
+        # return res
+
+        if not node:
+            return []
+            
+        stack1=[root]
+        stack2=[]
+
+        while stack1:
+            node=stack1.pop()
+            stack2.append(node)
+
+            if node.left:
+                stack1.append(node.left)
+            if node.right:
+                stack1.append(node.right)
+
+        while stack2:
+            node=stack2.pop()
+            res.append(node.val)
+
         return res
