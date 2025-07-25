eb14ea7de47769d677c40dd94cf332456d0f4957
Suhaas <53684099+Suhaas15@users.noreply.github.com>
2025-07-24 19:10:59 -0700

    Update L226_InvertBT.py

--- L226_InvertBT.py
+++ L226_InvertBT.py
@@ -1,3 +1,4 @@
+from collections import deque
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -6,12 +7,29 @@
 #         self.right = right
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        def swap(node):
-            if not node:
-                return 
-            node.left,node.right=node.right,node.left
-            left=swap(node.left)
-            right=swap(node.right)
+        # def swap(node):
+        #     if not node:
+        #         return 
+        #     node.left,node.right=node.right,node.left
+        #     left=swap(node.left)
+        #     right=swap(node.right)
 
-        swap(root)
+        # swap(root)
+        # return root
+
+        if not root:
+            return
+        
+        q=deque([root])
+
+        while q:
+            node=q.popleft()
+
+            node.left,node.right=node.right,node.left
+
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        
         return root
