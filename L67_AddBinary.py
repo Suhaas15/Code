aa295e593eabacb2b85f4433af742b1a2b68d626
Suhaas Srungavarapu <suhaas15032001@gmail.com>
2025-11-18 21:08:50 -0800

    Create L67_AddBinary.py

---
+++ L67_AddBinary.py
@@ -0,0 +1,10 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        num_a = int(a, 2)
+        num_b = int(b, 2)
+
+        sum_num = num_a + num_b
+
+        result = bin(sum_num)[2:]
+
+        return result
