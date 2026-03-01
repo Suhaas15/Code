c65bbc87c8e61b5927c606eb9ee237ce51c8e9c1
Suhaas Srungavarapu <suhaas15032001@gmail.com>
2026-02-28 19:41:41 -0800

    Implement minPartitions method to find max digit

---
+++ L1689_PartitioningIntoMinNumberOfDeciBinaryNums.py
@@ -0,0 +1,9 @@
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        # maxi=0
+        # for char in n:
+        #     maxi=max(maxi,int(char))
+        
+        # return maxi
+
+        return int(max(str(n)))
