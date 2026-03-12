be9f3e28a6dc0ece39a731b1c145a3275b79702f
Suhaas Srungavarapu <suhaas15032001@gmail.com>
2026-03-12 11:04:05 -0700

    Update L322_CoinChange.py

--- L322_CoinChange.py
+++ L322_CoinChange.py
@@ -1,11 +1,30 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        dp=[amount+1]*(amount+1)
-        dp[0]=0
+        # dp=[amount+1]*(amount+1)            #bottom up
+        # dp[0]=0
 
-        for a in range(1,amount+1):
-            for c in coins:
-                if a-c>=0:
-                    dp[a]=min(dp[a], 1+dp[a-c])
+        # for a in range(1,amount+1):
+        #     for c in coins:
+        #         if a-c>=0:
+        #             dp[a]=min(dp[a], 1+dp[a-c])
         
-        return dp[amount] if dp[amount]!=amount+1 else -1
+        # return dp[amount] if dp[amount]!=amount+1 else -1
+
+        memo={}                                #top-down
+        
+        def dfs(amount):
+            if amount==0:
+                return 0
+            if amount in memo:
+                return memo[amount]
+            
+            res=float('inf')
+            for coin in coins:
+                if amount-coin>=0:
+                    res = min(res, 1+dfs(amount-coin))
+            
+            memo[amount]=res
+            return res
+        
+        minCoins=dfs(amount)
+        return -1 if minCoins==float('inf') else minCoins
