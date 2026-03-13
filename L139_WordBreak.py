eb435e00f910d3130c325246d5f1101b6eaf2262
Suhaas Srungavarapu <suhaas15032001@gmail.com>
2026-03-12 18:57:49 -0700

    Update L139_WordBreak.py

--- L139_WordBreak.py
+++ L139_WordBreak.py
@@ -1,13 +1,29 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        dp=[False]*(len(s)+1)
-        dp[len(s)]=True
+        # dp=[False]*(len(s)+1)
+        # dp[len(s)]=True
 
-        for i in range(len(s)-1,-1,-1):
-            for w in wordDict:
-                if i+len(w)<=len(s) and s[i:i+len(w)]==w:
-                    dp[i] = dp[i+len(w)]
-                if dp[i]:
-                    break
+        # for i in range(len(s)-1,-1,-1):
+        #     for w in wordDict:
+        #         if i+len(w)<=len(s) and s[i:i+len(w)]==w:
+        #             dp[i] = dp[i+len(w)]
+        #         if dp[i]:
+        #             break
         
-        return dp[0]
+        # return dp[0]
+
+        memo={len(s): True}
+
+        def dfs(i):
+            if i in memo:
+                return memo[i]
+            
+            for w in wordDict:
+                if (i+len(w)<=len(s)) and (s[i:i+len(w)]==w):
+                    if dfs(i+len(w)):
+                        memo[i]=True
+                        return True
+            memo[i]=False
+            return False
+        
+        return dfs(0)
