59140b07f47bdd96c62bcf06d3c7a26e9a405d9a
Suhaas Srungavarapu <suhaas15032001@gmail.com>
2026-04-18 19:15:32 -0700

    Create L1855_MaxDistBwAPairOfValues.py

---
+++ L1855_MaxDistBwAPairOfValues.py
@@ -0,0 +1,9 @@
+class Solution:
+    def maxDistance(self, nums1: List[int], nums2: List[int]) -> int:
+        i=j=0
+        
+        while i<len(nums1) and j<len(nums2):
+            i+=nums1[i]>nums2[j]
+            j+=1
+        
+        return max(0,j-i-1)
