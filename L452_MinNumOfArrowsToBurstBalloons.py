f1fb6495fcff843df86d77ed390daa0cf8296ffe
Suhaas Srungavarapu <suhaas15032001@gmail.com>
2026-05-12 18:42:31 -0700

    Create L452_MinNumOfArrowsToBurstBalloons.py

---
+++ L452_MinNumOfArrowsToBurstBalloons.py
@@ -0,0 +1,15 @@
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        if not points:
+            return 0
+
+        points.sort(key=lambda pair:pair[1])
+        arrows=1
+        current_end=points[0][1]
+
+        for start, end in points[1:]:
+            if start>current_end:
+                arrows+=1
+                current_end=end
+        
+        return arrows
