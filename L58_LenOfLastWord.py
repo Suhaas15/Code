43b91517337cd296b3d40764879259a5aef71938
Suhaas Srungavarapu <suhaas15032001@gmail.com>
2026-06-08 18:47:15 -0700

    Update L58_LenOfLastWord.py

--- L58_LenOfLastWord.py
+++ L58_LenOfLastWord.py
@@ -1,4 +1,11 @@
 class Solution:
     def lengthOfLastWord(self, s: str) -> int:
-        list=s.split()
-        return len(list[-1])
+        i, length = len(s)-1,0
+        while s[i]==" ":
+            i-=1
+        
+        while i>=0 and s[i]!=" ":
+            i-=1
+            length+=1
+        
+        return length
