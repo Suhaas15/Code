b9a37d3c1d3c0b537e625a7cef429a74a6058297
Suhaas Srungavarapu <suhaas15032001@gmail.com>
2026-07-23 11:51:05 -0700

    Create L1394_FindLuckyIntInAnArr.py

---
+++ L1394_FindLuckyIntInAnArr.py
@@ -0,0 +1,11 @@
+from collections import Counter
+class Solution:
+    def findLucky(self, arr: List[int]) -> int:
+        freq=Counter(arr)
+
+        ans=-1
+        for key, val in freq.items():
+            if key==val:
+                ans=max(ans, key)
+        
+        return ans
