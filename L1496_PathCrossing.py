067cc023ac2962ac6592d4a951a4905526fa3560
Suhaas Srungavarapu <suhaas15032001@gmail.com>
2026-08-05 17:00:11 -0700

    Create L1496_PathCrossing.py

---
+++ L1496_PathCrossing.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPathCrossing(self, path: str) -> bool:
+        x,y = 0,0
+        visited={(0,0)}
+
+        for char in path:
+            if char=="N":
+                x+=1
+            elif char=="S":
+                x-=1
+            elif char=="E":
+                y+=1
+            else:
+                y-=1
+        
+            if (x,y) in visited:
+                return True
+            visited.add((x,y))
+        
+        return False
